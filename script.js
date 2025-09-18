f1562686c3a4991fc9542aca095d70b640f0d952
KALIKIRI HEMA <146416137+KalikiriHema@users.noreply.github.com>
2025-09-18 23:07:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Burger menu toggle for mobile
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
